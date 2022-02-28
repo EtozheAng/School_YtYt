7daafa8d6c023118a51e7a6c531c71cc4d0fafa4
EtozheAng <pavel.khisamutdinov@yandex.ru>
2022-02-28 19:24:18 +0300

    main part of the site

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
  $(function(){
+   $('.education__video-link').magnificPopup({
+     disableOn: 700,
+     type: 'iframe',
+     mainClass: 'mfp-fade',
+     removalDelay: 160,
+     preloader: false,
 
+     fixedContentPos: false
+   });
  });
