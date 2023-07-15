6282045d3e999cbe9c8158c666f39eeb59a91cff
EtozheAng <pavel.khisamutdinov@yandex.ru>
2023-07-15 15:37:44 +0300

    Changed readme.md

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,40 @@
-$(function(){
-   $(".menu a").on("click", function (event) {
-     event.preventDefault();
-     let id = $(this).attr('href'),
-       top = $(id).offset().top;
-     $('body,html').animate({ scrollTop: top }, 1500);
-   });
+$(function () {
+  $('.menu a').on('click', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
 
-   $('.education__video-link').magnificPopup({
-     disableOn: 700,
-     type: 'iframe',
-     mainClass: 'mfp-fade',
-     removalDelay: 160,
-     preloader: false,
-     fixedContentPos: false
-   });
+  $('.education__video-link').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  })
 
-   $('.menu__btn').on('click', function(){
-     $('.menu__list').addClass('menu__list--active');
-     $('.header__top').addClass('header__top--off-blur');
-     $('.btn-input__entry--active').fadeIn();
-     $('html').addClass('no-scroll');
-     $('.menu').addClass('menu__wisible');
-     $('.menu__closet').addClass('menu__wisible');
-   });
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').addClass('menu__list--active')
+    $('.header__top').addClass('header__top--off-blur')
+    $('html').addClass('no-scroll')
+    $('.menu').addClass('menu__wisible')
+    $('.menu__closet').addClass('menu__wisible')
+  })
 
-   $('.menu__closet').on('click', () => {
-     $('.menu__list').removeClass('menu__list--active');
-     $('.header__top').removeClass('header__top--off-blur');
-     $('html').removeClass('no-scroll');
-     $('.menu').removeClass('menu__wisible');
-     $('.menu__closet').removeClass('menu__wisible');
-   })
+  $('.menu__closet').on('click', () => {
+    $('.menu__list').removeClass('menu__list--active')
+    $('.header__top').removeClass('header__top--off-blur')
+    $('html').removeClass('no-scroll')
+    $('.menu').removeClass('menu__wisible')
+    $('.menu__closet').removeClass('menu__wisible')
+  })
 
-   $('.menu__list-item').on('click', () => {
-     $('.menu').removeClass('menu__wisible'); 
-   })
-  });
-
-
+  $('.menu__list-item').on('click', () => {
+    $('.menu__list').removeClass('menu__list--active')
+    $('.header__top').removeClass('header__top--off-blur')
+    $('html').removeClass('no-scroll')
+    $('.menu').removeClass('menu__wisible')
+  })
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel, series, task } = require('gulp')
+const ghPages = require('gulp-gh-pages')
 
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
-const browserSync  = require('browser-sync').create();
-
+const scss = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const del = require('del')
+const browserSync = require('browser-sync').create()
 
 function browsersync() {
   browserSync.init({
     server: {
-    baseDir: 'app/'
+      baseDir: 'app/',
     },
-    notofy: false
+    notofy: false,
   })
 }
 
-function styles() {  
+task('deploy', function () {
+  return src('./dist/**/*').pipe(ghPages())
+})
+
+function styles() {
   return src('app/scss/style.scss')
-  .pipe(scss({outputStyle: 'expanded'}))
-  .pipe(concat('style.min.css'))
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 10 version'],
-    grid: true
-  }))
-  .pipe(dest('app/css'))
-  .pipe(browserSync.stream())
+    .pipe(scss({ outputStyle: 'expanded' }))
+    .pipe(concat('style.min.css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true,
+      })
+    )
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function scripts() {
@@ -35,37 +41,33 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'app/js/main.js',
-    'app/js/quiz.js'
+    'app/js/quiz.js',
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
-function images(){
+function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({ interlaced: true }),
-    imagemin.mozjpeg({ quality: 75, progressive: true }),
-    imagemin.optipng({ optimizationLevel: 5 }),
-    imagemin.svgo({
-      plugins: [
-        { removeViewBox: true },
-        { cleanupIDs: false }
-      ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest('dist/images'))
 }
 
 function building() {
-  return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base: 'app'})
-    .pipe(dest('dist'))
+  return src(['app/**/*.html', 'app/css/style.min.css', 'app/js/main.min.js'], {
+    base: 'app',
+  }).pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -73,18 +75,17 @@ function cleanDist() {
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/**/*.scss'], styles)
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+  watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
+exports.styles = styles
+exports.scripts = scripts
+exports.browsersync = browsersync
+exports.watching = watching
+exports.images = images
+exports.cleanDist = cleanDist
+exports.build = series(cleanDist, images, building)
 
-exports.styles = styles;
-exports.scripts = scripts; 
-exports.browsersync = browsersync;
-exports.watching = watching;
-exports.images = images;
-exports.cleanDist = cleanDist;
-exports.build = series(cleanDist, images, building);
-
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, browsersync, watching)
